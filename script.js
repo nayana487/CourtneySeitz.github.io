25961cd416d7946911c17001fbd24c2bb83fb1d6
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-30 14:14:43 -0400

    jquery test

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   $('.fade').fadeIn(3000);
-  $(".body").hide();
-  $(".footer").hide();
+//  $(".body").hide();
+//  $(".footer").hide();
 });
 
-$(".test").click(function(){
-  $(".body").fadeIn(2200);
-});
+//$(".test").click(function(){
+//  $(".body").fadeIn(2200);
+//  $(".footer")fadeIn(2200);
+//});
