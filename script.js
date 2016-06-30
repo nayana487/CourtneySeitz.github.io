de0b54b4dc4e06897b6480a35b3d10bdeccbfa04
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-30 13:11:41 -0400

    indentations fixes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // $(document).ready(function() {
-  //$(".fade").hide();
-  //$(".body").hide();
-  //$(".footer").hide();
+//$(".fade").hide();
+//$(".body").hide();
+//$(".footer").hide();
 //})
 
 // $(".test").click(function(){
-  //$(".fade").fadeIn("slow");
+//$(".fade").fadeIn("slow");
 //})
