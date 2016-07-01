2f2e61624d4223fb02a7cc29057cd12a5dfaae50
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-30 21:27:43 -0400

    last bits

--- script.js
+++ script.js
@@ -12,28 +12,28 @@ $(document).ready(function() {
 //$(".body").fadeIn(2200);
 //});
 
-//function for sticking nav bar as window scrolls
+//function for sticking nav bar to the top as the window scrolls
 $(function() {
     var nav = $('#navBarFloat');
     var navHome = nav.offset().top;
     var isFixed = false;
     var scrollWindow = $(window);
-    scrollWindow.scroll(function() {
-        var scrollTop = scrollWindow.scrollTop();
-        var shouldBeFixed = scrollTop > navHome;
+scrollWindow.scroll(function() {
+    var scrollTop = scrollWindow.scrollTop();
+    var shouldBeFixed = scrollTop > navHome;
         if (shouldBeFixed && !isFixed) {
-            nav.css({
-              position: 'fixed',
-              top: 0,
-              left: nav.offset().left,
-              width: nav.width()
+          nav.css({
+            position: 'fixed',
+            top: 0,
+            left: nav.offset().left,
+            width: nav.width()
             });
             isFixed = true;
         }
         else if (!shouldBeFixed && isFixed)
         {
             nav.css({
-                position: 'static'
+            position: 'static'
             });
             isFixed = false;
         }
